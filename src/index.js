e915e05336d954846a5c33d6092d86e2a2ab2b03
Abhinay sharma <abhinaysharma127@gmail.com>
2022-06-20 23:26:35 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <Router basename={"/react-bookstore-cart"}>
+  <Router basename={"/online_bookstore"}>
     <App />
   </Router>,
   document.getElementById("root")
